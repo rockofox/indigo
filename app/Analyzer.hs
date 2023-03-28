7c1682711f79380ee52f64c928b73d31f0d92223
rockofox <ofelix@pm.me>
2023-03-29 00:14:23 +0200

    Check number of arguments

--- app/Analyzer.hs
+++ app/Analyzer.hs
@@ -7,7 +7,7 @@ import Data.List (intercalate, partition)
 import Parser
 
 isFuncDef :: Expr -> Bool
-isFuncDef (FuncDef {}) = True
+isFuncDef (FuncDef{}) = True
 isFuncDef _ = False
 
 isFuncDec :: Expr -> Bool
@@ -21,41 +21,42 @@ isFuncCall _ = False
 analyseProgram :: Program -> String
 analyseProgram (Program exprs) = do
   intercalate "\n" (map analyseExpression exprs)
-  where
-    (funcDecs, _) = partition isFuncDec exprs
+ where
+  (funcDecs, _) = partition isFuncDec exprs
 
-    analyseExpression :: Expr -> String
-    analyseExpression (Var name) = name
-    analyseExpression (BoolLit True) = "true"
-    analyseExpression (BoolLit False) = "false"
-    analyseExpression (IntLit n) = show n
-    analyseExpression (BinOp op left right) = "(" ++ analyseExpression left ++ " " ++ op ++ " " ++ analyseExpression right ++ ")"
-    analyseExpression (If cond thenExpr elseExpr) =
-      "if "
-        ++ analyseExpression cond
-        ++ " {\n"
-        ++ analyseExpression thenExpr
-        ++ "\n} else {\n"
-        ++ analyseExpression elseExpr
-        ++ "\n}"
-    analyseExpression (Let name val body) =
-      "let "
-        ++ name
-        ++ " = "
-        ++ analyseExpression val
-        ++ ";\n"
-        ++ analyseExpression body
-    analyseExpression (FuncDef name args body) = do
-      unless (any (\(FuncDec name' _) -> name == name') funcDecs) (error ("Function definition for nonexistant function: " ++ name))
-      "fn "
-        ++ name
-        ++ "("
-        ++ intercalate ", " args
-        ++ ") -> i32 {\n"
-        ++ analyseExpression body
-        ++ "\n}"
-    analyseExpression (FuncDec name types) = "// " ++ name ++ ":" ++ intercalate "," types
-    analyseExpression (FuncCall name args) = do
-      unless (any (\(FuncDec name' _) -> name == name') funcDecs) (error ("Function call to nonexistant function: " ++ name))
-      name ++ "(" ++ intercalate ", " (map analyseExpression args) ++ ")"
-    analyseExpression (DoBlock exprs) = intercalate "\n" (map analyseExpression exprs)
+  analyseExpression :: Expr -> String
+  analyseExpression (Var name) = name
+  analyseExpression (BoolLit True) = "true"
+  analyseExpression (BoolLit False) = "false"
+  analyseExpression (IntLit n) = show n
+  analyseExpression (BinOp op left right) = "(" ++ analyseExpression left ++ " " ++ op ++ " " ++ analyseExpression right ++ ")"
+  analyseExpression (If cond thenExpr elseExpr) =
+    "if "
+      ++ analyseExpression cond
+      ++ " {\n"
+      ++ analyseExpression thenExpr
+      ++ "\n} else {\n"
+      ++ analyseExpression elseExpr
+      ++ "\n}"
+  analyseExpression (Let name val body) =
+    "let "
+      ++ name
+      ++ " = "
+      ++ analyseExpression val
+      ++ ";\n"
+      ++ analyseExpression body
+  analyseExpression (FuncDef name args body) = do
+    unless (any (\(FuncDec name' _) -> name == name') funcDecs) (error ("Function definition for nonexistant function: " ++ name))
+    "fn "
+      ++ name
+      ++ "("
+      ++ intercalate ", " args
+      ++ ") -> i32 {\n"
+      ++ analyseExpression body
+      ++ "\n}"
+  analyseExpression (FuncDec name types) = "// " ++ name ++ ":" ++ intercalate "," types
+  analyseExpression (FuncCall name args) = do
+    unless (any (\(FuncDec name' _) -> name == name') funcDecs) (error ("Function call to nonexistant function: " ++ name))
+    unless (any (\(FuncDec name' types) -> name == name' && length args == length types) funcDecs) (error ("Function call to function with wrong number of arguments: " ++ name))
+    name ++ "(" ++ intercalate ", " (map analyseExpression args) ++ ")"
+  analyseExpression (DoBlock exprs) = intercalate "\n" (map analyseExpression exprs)
