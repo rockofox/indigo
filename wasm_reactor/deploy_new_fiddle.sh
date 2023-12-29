7972c26e9bb547c08a29594d43da8e03a5eb1dc4
rockofox <ofelix@pm.me>
2023-12-29 14:20:23 +0100

    wasm deployment script: npm update bulma/fiddle

--- wasm_reactor/deploy_new_fiddle.sh
+++ wasm_reactor/deploy_new_fiddle.sh
@@ -4,3 +4,7 @@ GITREV=$(git rev-parse --short HEAD)
 cd ../../indigo-fiddle || exit
 git commit -m "Update fiddle to indigo@$GITREV" indigo-init.wasm indigo-lib
 git push
+cd ../indigo-bulma || exit
+npm update indigo-fiddle
+git commit -m "Update fiddle to indigo@$GITREV" package-lock.json
+git push
