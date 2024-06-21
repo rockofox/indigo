aab42514e5efeadd23e001c586cccd5803759162
rockofox <ofelix@pm.me>
2024-06-21 18:52:50 +0200

    Fix wasm-reactor build

--- wasm_reactor/build-wasm.sh
+++ wasm_reactor/build-wasm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # TODO: Make sure this script is always run from wasm_reactor directory
-wasm32-wasi-cabal build exe:indigo-wasm-reactor --allow-newer='*' -f noffi
-INDIGO_WASM=$(realpath ./dist-newstyle/build/wasm32-wasi/ghc-*/indigo-wasm-reactor-*/x/indigo-wasm-reactor/build/indigo-wasm-reactor/indigo-wasm-reactor.wasm)
+wasm32-wasi-cabal build exe:indigo-wasm-reactor --allow-newer='base' -f noffi
+INDIGO_WASM=$(wasm32-wasi-cabal list-bin indigo-wasm-reactor --allow-newer=base | tail -n 1)
 wizer --allow-wasi --wasm-bulk-memory true "$INDIGO_WASM" -o indigo-init.wasm
 cp -f indigo-init.wasm ../../indigo-fiddle
 cp -rf ../share ../../indigo-fiddle/indigo-lib
