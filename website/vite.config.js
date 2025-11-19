a3663fcb1f16c8dcad326afed7eea8bc8ab4ec86
rockofox <5049120+rockofox@users.noreply.github.com>
2025-11-19 20:11:25 +0100

    website: fix base path

--- website/vite.config.js
+++ website/vite.config.js
@@ -3,5 +3,5 @@ import elmPlugin from 'vite-plugin-elm';
 
 export default defineConfig({
   plugins: [elmPlugin()],
-  base: '/indigo/', // GitHub Pages base path (repo name)
+  // base: '/indigo/', // GitHub Pages base path (repo name)
 });
